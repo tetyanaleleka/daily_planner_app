4196e308de839656b20a30b48510d55e73aa62f7
tetyanaleleka <tana.leleka@gmail.com>
2023-10-23 20:25:32 +0100

    Updated index.html, index.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,34 @@ $(document).ready(function(){
 
 function hourUpdate (){
     let currentHour = moment().hours();
-    console.log(currentHour);
+    
+   
+    for(let i = 0; i < $(".time-block").length; i++){
+        let hour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1])
+        console.log(hour)
+        console.log(currentHour)
+        if(hour < currentHour) {
+            $(".time-block")[i].classList.add("past")
+        } else if(hour === currentHour){
+            $(".time-block")[i].classList.add("past")
+            $(".time-block")[i].classList.remove("present")
+        } else {
+            $(".time-block")[i].classList.remove("past")
+            $(".time-block")[i].classList.remove("present")
+            $(".time-block")[i].classList.add("future")
+        }
+    }
 }
 
 hourUpdate();
+
+let interval = setInterval(hourUpdate, 15000);
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+
+$("#currentDay").text(moment().format("dddd, MMMM, Do"))
+
 })
